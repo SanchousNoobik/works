0b6b8e7a965b1c0c48ed2324e88fa008e0830586
SanchousNoobik <147470644+SanchousNoobik@users.noreply.github.com>
2023-12-23 15:25:37 +0500

    Add files via upload

---
+++ однокнопочный.py
@@ -0,0 +1,16 @@
+a=''
+while True:
+    n=input()
+    if n=='e':
+        b='A'
+        a=a+b
+    elif n=='eeeee':
+        c='E'
+        a=a+c
+    elif n=='eeeeeee':
+        g='G'
+        a=a+g
+    else:
+        break
+print(a)
+         
