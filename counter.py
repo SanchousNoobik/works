a869dc6c8e0954c3cc7f1927e9528f25d80d888b
SanchousNoobik <147470644+SanchousNoobik@users.noreply.github.com>
2023-12-25 15:59:43 +0500

    Delete counter.py

--- counter.py
+++
@@ -1,5 +0,0 @@
-from collections import Counter
-def top3(st):
-    counter_top3=Counter(st.replace(' ','')).most_common(3)
-    print(counter_top3)
-top3("sdfjdsfjdskljdslfjdjldjsdjflhfhhjsdsdhkfj")
