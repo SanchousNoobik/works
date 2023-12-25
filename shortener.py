93d2f44e7a27bfa0d4620d29f767488ded5e5cb7
SanchousNoobik <147470644+SanchousNoobik@users.noreply.github.com>
2023-12-25 16:00:27 +0500

    Delete shortener.py

--- shortener.py
+++
@@ -1,7 +0,0 @@
-def shortener(st):
-    while '(' in st or ')' in st:
-        left=st.rfind('(')
-        right=st.find(')', left)
-        st=st.replace(st[left:right + 1], '')
-        return st
-print(shortener('gggggggg(ffffff)'))
